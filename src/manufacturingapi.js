1978dab8b07ba64748af7a13a719a3392adb9b56
abhishek <shrivastavabhishek118@gmail.com>
2022-08-24 13:02:08 +0530

    update

--- src/manufacturingapi.js
+++ src/manufacturingapi.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React from "react";
 // import Advertisement from "./advertisement";
 // import Editior from "./editior";
-import axios from 'axios';
+// import axios from 'axios';
 
 export default function Manufacturingapi() {
  
